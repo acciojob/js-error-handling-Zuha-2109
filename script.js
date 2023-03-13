dab6ea44b3cc02e7f255e160802fa91b51719693
Zuha Khundmiri <70804630+Zuha-2109@users.noreply.github.com>
2023-03-13 17:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-//your code here
 class OutOfRangeError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'OutOfRangeError';
+  constructor(arg) {
+    super(`Expression should only consist of integers and +-/* characters and not < arg >`);
+    this.name = "OutOfRangeError";
   }
 }
 
+// Define InvalidExprError class
 class InvalidExprError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'InvalidExprError';
+  constructor() {
+    super("Expression should not have an invalid combination of expression");
+    this.name = "InvalidExprError";
   }
 }
 
-function evalString(expr) {
+// Define evalString function
+function evalString() {
+  const expression = document.getElementById("expression").value;
   try {
-    // Check for invalid operator combinations
-    if (/\+\+|--|\+\-|-\+|\*\/|\/\*/.test(expr)) {
-      throw new InvalidExprError('Expression should not have an invalid combination of operators');
+    // Check for invalid expression combinations
+    if (/[+\-*/]{2,}/.test(expression)) {
+      throw new InvalidExprError();
     }
 
-    // Check for invalid start and end operators
-    if (/^[+/*]/.test(expr)) {
-      throw new SyntaxError('Expression should not start with invalid operator');
-    }
-    if (/[\+\-*/]$/.test(expr)) {
-      throw new SyntaxError('Expression should not end with invalid operator');
+    // Check for invalid starting operator
+    if (/^[+\-*/]/.test(expression)) {
+      throw new SyntaxError("Expression should not start with invalid operator");
     }
 
-    // Check for valid expression format
-    if (!/^[-+*/\d\s()]+$/.test(expr)) {
-      throw new OutOfRangeError('Expression should only consist of integers and +-/* characters');
+    // Check for invalid ending operator
+    if (/[+\-*/]$/.test(expression)) {
+      throw new SyntaxError("Expression should not end with invalid operator");
     }
 
-    // Evaluate the expression
-    return eval(expr);
+    // Evaluate expression
+    const result = eval(expression);
+
+    // Check for OutOfRangeError
+    if (!Number.isInteger(result)) {
+      throw new OutOfRangeError(result);
+    }
+
+    alert(`Result: ${result}`);
   } catch (error) {
-    throw error;
+    if (error instanceof OutOfRangeError || error instanceof InvalidExprError) {
+      alert(`Error: ${error.message}`);
+    } else {
+      alert(`Syntax Error: ${error.message}`);
+    }
   }
 }
